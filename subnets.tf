e69f9aa6af8d57690384f27439c6bf44fe9ba534
CodingManoj <mvp2612@hotmail.com>
2023-09-20 22:48:12 -0400

    Git Auto Push

--- subnets.tf
+++ subnets.tf
@@ -8,3 +8,15 @@ resource "aws_subnet" "public_subnet" {
     Name = "roboshop-${var.ENV}-pub-subnet-${element(var.AZ, count.index)}"
   }
 }
+
+
+resource "aws_subnet" "private_subnet" {
+  count                 = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id                = aws_vpc.main.id
+  cidr_block            = element(var.PRIVATE_SUBNET_CIDR, count.index) 
+  availability_zone     = element(var.AZ, count.index)
+
+  tags = {
+    Name = "roboshop-${var.ENV}-pprv-subnet-${element(var.AZ, count.index)}"
+  }
+}
