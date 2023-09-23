79921c9abb68502c77388d70cb1c855d13ea3ed6
CodingManoj <mvp2612@hotmail.com>
2023-09-22 22:47:11 -0400

    Git Auto Push

--- gw.tf
+++ gw.tf
@@ -19,7 +19,7 @@ resource "aws_eip" "ngw_ip" {
 
 # Creates NAT Gateway and will be attached to Public Subnet
 resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.example.id
+  allocation_id = aws_eip.ngw_ip.id
   subnet_id     = aws_subnet.public_subnet.*.id[0] 
 
   tags = {
