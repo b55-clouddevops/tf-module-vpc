2e2dbd355bf58702cfe962f50295bf62e4b83604
CodingManoj <mvp2612@hotmail.com>
2023-09-22 22:22:45 -0400

    Git Auto Push

--- peering.tf
+++ peering.tf
@@ -2,5 +2,5 @@
 resource "aws_vpc_peering_connection" "peer" {
   vpc_id      = var.DEFAULT_VPC_ID
   peer_vpc_id = aws_vpc.main.id
-  auto_accept = true
+  auto_accept = true                                 # This is valid if both the VPC's are in the same account.
 }
