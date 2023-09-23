d3da13815948068841bb7444972b0c447fd6f84e
CodingManoj <mvp2612@hotmail.com>
2023-09-22 22:58:17 -0400

    Git Auto Push

--- route_table.tf
+++ route_table.tf
@@ -54,3 +54,12 @@ resource "aws_route_table_association" "private_rt_association" {
   subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.private_rt.id
 }
+
+
+# Adding Route To THe Default VPC Route Table 
+resource "aws_route" "default_vpc_rote" {
+  route_table_id            = var.DEFAILT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  depends_on                = [aws_vpc_peering_connection.peer]
+}
